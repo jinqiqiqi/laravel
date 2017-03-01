a5ecc2dbead5813e3db4367db40087d5bbe59b49
eric.king <jinqiqiqi@gmail.com>
2017-03-01 16:55:01 +0800

    add html and form builders

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         Furbook\Providers\EventServiceProvider::class,
         Furbook\Providers\RouteServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +227,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
 
     ],
 
